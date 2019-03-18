0126bd1377c22ab0e8fa3c7931a819e45f002c40
Carlo de los Angeles <carlo.delosangeles@gmail.com>
2019-03-18 11:47:49 -0700

    added pmod_design.m

---
+++ brainImaging/mri/fmri/glmActivation/pmod/pmod_design.m
@@ -0,0 +1,135 @@
+% This script adds parametric modulation for an existing task design
+% _________________________________________________________________________
+% 2019 Stanford Cognitive and Systems Neuroscience Laboratory
+%
+% $Id:  $ 03-18-19 Carlo de los Angeles
+
+% -------------------------------------------------------------------------
+
+function pmod_design(subject_i,ConfigFile)
+
+warning('off', 'MATLAB:FINITE:obsoleteFunction')
+c     = fix(clock);
+disp('==================================================================');
+fprintf('Parametric Modulation starts at %d/%02d/%02d %02d:%02d:%02d \n',c);
+disp('==================================================================');
+
+currentdir = pwd;
+
+% -------------------------------------------------------------------------
+% Check existence of the configuration file
+% -------------------------------------------------------------------------
+
+ConfigFile = strtrim(ConfigFile);
+if ~exist(ConfigFile,'file')
+    fprintf('Cannot find the configuration file ... \n');
+    diary off;
+    return;
+end
+
+ [ConfigFilePath, ConfigFile, ConfigFileExt] = fileparts(ConfigFile);
+    eval(ConfigFile);
+    clear ConfigFile;
+
+spm_version             = strtrim(paralist.spmversion);
+software_path           = '/oak/stanford/groups/menon/toolboxes/';
+spm_path                = fullfile(software_path, spm_version);
+
+spm_version             = strtrim(paralist.spmversion);
+software_path           = '/oak/stanford/groups/menon/toolboxes/';
+spm_path                = fullfile(software_path, spm_version);
+spmindvstatsscript_path   = ['/oak/stanford/groups/menon/scsnlscripts/brainImaging/mri/fmri/glmActivation/individualStats/' spm_version];
+
+sprintf('adding SPM path: %s\n', spm_path);
+addpath(genpath(spm_path));
+
+% -------------------------------------------------------------------------
+% Read individual stats parameters
+% -------------------------------------------------------------------------
+% Ignore white space if there is any
+    subject_i          = SubjectI;
+    subjectlist        = strtrim(paralist.subjectlist);
+    runlist            = strtrim(paralist.runlist);
+    project_dir        = strtrim(paralist.projectdir);
+    task_dsgn          = strtrim(paralist.task_dsgn);
+    pmod_script		   = strtrim(paralist.pmod_script)
+
+
+    [v,r] = spm('Ver','',1);
+    fprintf('>>>-------- This SPM is %s V%s ---------------------\n',v,r);
+
+    disp('-------------- Contents of the Parameter List --------------------');
+    disp(paralist);
+    disp('------------------------------------------------------------------');
+    clear paralist;
+
+% -------------------------------------------------------------------------
+% Read in subjects and sessions
+% Get the subjects, sesses in cell array format
+        subjectlist       = csvread(subjectlist,1);
+        subject           = subjectlist(subject_i);
+        subject           = char(string(subject));
+        subject           = char(pad(string(subject),4,'left','0'));%<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        visit             = num2str(subjectlist(subject_i,2));
+        session           = num2str(subjectlist(subject_i,3));
+
+        numsub           = 1;
+        runs              = ReadList(runlist);
+        numrun            = length(runs);
+
+
+
+% -------------------------------------------------------------------------
+% Start pmod task design processing
+% -------------------------------------------------------------------------
+for subcnt = 1:numsub
+    
+	fprintf('Processing Subject: %s \n',subject);
+	disp('--------------------------------------------------------------');
+
+	run_raw_dir = cell(numrun,1);
+
+	for irun = 1:numrun
+      
+	    %--------  run folder (preprocessed)
+	     run_raw_dir{irun} = fullfile(project_dir, '/data/imaging/participants/', subject, ...
+	                        ['visit',visit], ['session',session], 'fmri', runs{irun});
+
+	    %-------- Check task_design file 
+	    addpath(fullfile(run_raw_dir{irun}, 'task_design'));
+	    str = which(task_dsgn);
+	    fprintf('>>>> str is %s \n',str);
+	    if isempty(str)
+	       error('Cannot find task design file in task_design folder.');
+	       cd(currentdir);
+	       return;
+	    end
+	                    
+		%%%-------------load task design file ---------------
+
+		[filepath,name,ext] = fileparts(task_dsgn);
+		fprintf('>>> pwd is %s \n',pwd);
+		fprintf('>>>> task_design file is %s %s %s \n',filepath, name, ext);
+		if(strcmp(ext,'.mat'))
+		    load(str);
+		    fprintf('<><> \n');
+		else
+		    error('task design file type should be *.mat');
+		end
+		    rmpath(fullfile(run_raw_dir{irun}, 'task_design'));
+
+	    %%%-------------add pmods to task design file --------
+
+	    [pmod_filepath,name,ext] = fileparts(pmod_script)
+	    addpath(pmod_filepath)
+
+	    [sess_name names onsets durations pmod rest_exists] = pmod(sess_name names onsets durations pmod rest_exists)
+
+	    %%% ------------resave task_design with pmods file --------------------
+
+	    cd(fullfile(run_raw_dir{irun}, 'task_design'))
+	    save task_design_pmod.mat sess_name names onsets durations pmod rest_exists
+
+
+	end
+end
