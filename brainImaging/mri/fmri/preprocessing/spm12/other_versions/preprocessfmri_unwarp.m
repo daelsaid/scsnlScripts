134304fead6a079e45fe38abaa76db954bdb55ad
daelsaid <daelsaid@stanford.edu>
2019-04-03 09:39:37 -0700

    workable function

--- brainImaging/mri/fmri/preprocessing/spm12/other_versions/preprocessfmri_unwarp.m
+++ brainImaging/mri/fmri/preprocessing/spm12/other_versions/preprocessfmri_unwarp.m
@@ -1,9 +1,10 @@
 function preprocessfmri_unwarp(run_name,subj_prefix,subject,visit,session,temp_dir,project_dir)
-
+    
     addpath(genpath('/home/users/daelsaid/bin/afni_local/'));
 
     unwarp_script      = ('/oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/scsnl_script_implementation/unwarpepi.py');
     raw_dir            = ('/oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/scsnl_script_implementation/subj/');
+    
     unnormed_dirname   = 'unnormalized';
     data_dir           = fullfile(project_dir,'/data/imaging/participants/',subject,['visit',visit],['session',session],run_name);
     unnorm_dir         = fullfile(raw_dir, subject, ['visit',visit],['session',session],'fmri', run_name, unnormed_dirname);
@@ -11,9 +12,9 @@ function preprocessfmri_unwarp(run_name,subj_prefix,subject,visit,session,temp_d
     temp_nii           = fullfile(temp_dir,[subj_prefix,'.nii']);
 
     pepolar_suffix     = '_pepolar';
-    pepolar_volumes    = "'[0..20]'";
     pepolar_dirname    = [run_name,pepolar_suffix];
     pepolar_dir        = fullfile(raw_dir,subject,['visit',visit],['session',session],'fmri',pepolar_dirname);
+    
     pepolar_orig_nii   = fullfile(pepolar_dir,unnormed_dirname,'I.nii.gz')
     pepolar_gzipnii    = fullfile(temp_dir,[subj_prefix,'_rev.nii.gz']);
     pepolar_nii	       = fullfile(temp_dir,[subj_prefix,'_rev.nii']);
@@ -25,9 +26,9 @@ function preprocessfmri_unwarp(run_name,subj_prefix,subject,visit,session,temp_d
     if ~exist(pepolar_dir, 'dir')
         fprintf('Cannot find the PE Polar folder: %s\n', pepolar_dir);
     end
-
-    %cd(fullfile('/scratch/users/daelsaid/tmp_files',[subject,'visit',visit,'session',session,run_name,'_']));
+    
     cd(temp_dir);
-    call_unwarpepi=['python ', unwarp_script,[' -f ',subj_prefix,'.nii', pepolar_volumes], [' -r ', subj_prefix,'_rev.nii'], [' -d ', subj_prefix,'.nii'],' -s ', subj_prefix]
-    unix(sprintf('%s', call_unwarpepi));
+    unix(sprintf('python %s -f %s -r %s -d %s',unwarp_script,[subj_prefix '.nii''[0..20]'''], [subj_prefix '_rev.nii'], [subj_prefix '.nii']));
+    
+    sprintf('python %s -f %s -r %s -d %s -s %s',unwarp_script,[subj_prefix '.nii''[0..20]'''], [subj_prefix '_rev.nii'], [subj_prefix '.nii'], subject)
 end
