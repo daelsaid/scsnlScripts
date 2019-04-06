61fc6af4c6d7fd0fa4c04b69469f3878114e7851
daelsaid <daelsaid@stanford.edu>
2019-04-05 17:30:59 -0700

    updated unwarping- changed method of calling python script

--- brainImaging/mri/fmri/preprocessing/spm12/other_versions/preprocessfmri_unwarp.m
+++ brainImaging/mri/fmri/preprocessing/spm12/other_versions/preprocessfmri_unwarp.m
@@ -1,10 +1,8 @@
 function preprocessfmri_unwarp(run_name,subj_prefix,subject,visit,session,temp_dir,project_dir)
-    
     addpath(genpath('/home/users/daelsaid/bin/afni_local/'));
-
+    
     unwarp_script      = ('/oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/scsnl_script_implementation/unwarpepi.py');
     raw_dir            = ('/oak/stanford/groups/menon/projects/daelsaid/2019_met_unwarpepi/scsnl_script_implementation/subj/');
-    
     unnormed_dirname   = 'unnormalized';
     data_dir           = fullfile(project_dir,'/data/imaging/participants/',subject,['visit',visit],['session',session],run_name);
     unnorm_dir         = fullfile(raw_dir, subject, ['visit',visit],['session',session],'fmri', run_name, unnormed_dirname);
@@ -14,7 +12,6 @@ function preprocessfmri_unwarp(run_name,subj_prefix,subject,visit,session,temp_d
     pepolar_suffix     = '_pepolar';
     pepolar_dirname    = [run_name,pepolar_suffix];
     pepolar_dir        = fullfile(raw_dir,subject,['visit',visit],['session',session],'fmri',pepolar_dirname);
-    
     pepolar_orig_nii   = fullfile(pepolar_dir,unnormed_dirname,'I.nii.gz')
     pepolar_gzipnii    = fullfile(temp_dir,[subj_prefix,'_rev.nii.gz']);
     pepolar_nii	       = fullfile(temp_dir,[subj_prefix,'_rev.nii']);
@@ -28,7 +25,5 @@ function preprocessfmri_unwarp(run_name,subj_prefix,subject,visit,session,temp_d
     end
     
     cd(temp_dir);
-    unix(sprintf('python %s -f %s -r %s -d %s',unwarp_script,[subj_prefix '.nii''[0..20]'''], [subj_prefix '_rev.nii'], [subj_prefix '.nii']));
-    
-    sprintf('python %s -f %s -r %s -d %s -s %s',unwarp_script,[subj_prefix '.nii''[0..20]'''], [subj_prefix '_rev.nii'], [subj_prefix '.nii'], subject)
+    system(sprintf('python %s -f %s -r %s -d %s',unwarp_script,[subj_prefix '.nii' '[' '0..20' ']'],[subj_prefix '_rev.nii'], [subj_prefix '.nii']));
 end
