026634e00f3643740e9153fb93797785301b4ccb
daelsaid <daelsaid@stanford.edu>
2019-04-29 16:29:37 -0700

    updated and final scripts for unwarp pipeline

--- brainImaging/mri/fmri/preprocessing/spm12/utils/convert_unwarpmov_params.m
+++ brainImaging/mri/fmri/preprocessing/spm12/utils/convert_unwarpmov_params.m
@@ -1,13 +1,32 @@
 function convert_unwarpmov_params(temp_dir,unwarp_motion_file)
-rp_outfile='rp_I.txt';
-unwarped_motion_params=load(unwarp_motion_file,"-ascii")
+%      daelsaid 04/29/2019: this script takes an afni motion file and converts
+%      the rotation parameters from degrees to radians and orders the columns
+%      to match spm's order (from z,x,y to x,y,z).
+%
+%     input
+%         temp_dir= working directory of current task being run through the preprocessfmri_distortioncorr.m pipeline
+%         unwarp_motion_file =  afni motion file generated via unwarpEPIpipeline.
+%              - requires that the first two rows are removed
+%     output
+%          rp_I.txt file - for easy re-integration to
+%          preprocessfmri pipeline (unwarp_motion_file_convert.sh can
+%          generate the appropriate output file)
+%
 
-rotation_x=degtorad(unwarped_motion_params(:,6));
-rotation_y=degtorad(unwarped_motion_params(:,5));
-rotation_z=degtorad(unwarped_motion_params(:,4));
+rp_outfile='rp_I.txt'; %output filename
 
+unwarped_motion_params=load(unwarp_motion_file,"-ascii") %load the motion file
+
+%convert units from angle to radians
+rotation_x=deg2rad(unwarped_motion_params(:,6)); % x-angle
+rotation_y=deg2rad(unwarped_motion_params(:,5)); % y-angle
+rotation_z=deg2rad(unwarped_motion_params(:,4)); % z-angle
+
+% combine translation param columns with re-ordered rotation columns
 orderedparams=[unwarped_motion_params(:,1:3),rotation_x,rotation_y,rotation_z];
 
+%write new params to file, using tabs as the delimiter, and in scientific
+%notation with 6 decimal places
 dlmwrite(rp_outfile,orderedparams,'delimiter','\t','precision','% .6e')
 
 end
--- brainImaging/mri/fmri/preprocessing/spm12/utils/unwarp_motionfile_convert.sh
+++ brainImaging/mri/fmri/preprocessing/spm12/utils/unwarp_motionfile_convert.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
 #daelsaid 04/12/2019
-# script takes AFNI's UnWarpEPI motion file (.motion.1D) and converts it to an SPM formatted motion file
+# this script takes AFNI's UnWarpEPI motion file (.motion.1D) and reformats column spacing to match SPM's rp alignment output file.
 Usage() {
     echo "Usage: unwarp_motionfile_convert.sh <file_to_convert>"
     echo ""
     echo "<file_to_convert> full path to file including filename, or filename"
     echo ""
+    echo "outputfile: orig_filename+_nohdr"
     exit 1;
 }
 
@@ -18,8 +18,10 @@ fi
 file_to_convert=$1
 file_dir=$(dirname ${file_to_convert})
 
+#make a copy of orig file
 cp ${file_to_convert} ${file_to_convert}_
-sed 1d ${file_to_convert}_ >> ${file_to_convert}_tmp1;
-sed 1d ${file_to_convert}_tmp1 >> ${file_to_convert}_nohdr_tmp;
+sed 1d ${file_to_convert}_ >> ${file_to_convert}_tmp1; #remove row 1
+sed 1d ${file_to_convert}_tmp1 >> ${file_to_convert}_nohdr_tmp; #remove row 2
 
-cat ${file_to_convert}_nohdr_tmp | sed 's/ /   /g' | sed 's/ -/-/g' > ${file_to_convert}_nohdr
+cat ${file_to_convert}_nohdr_tmp | sed 's/ /   /g' | sed 's/ -/-/g' > ${file_to_convert}_nohdr #adjust column spacing and padding and write to file
+rm -rf ${file_to_convert}_ ${file_to_convert}_tmp1 #remove intermediates
